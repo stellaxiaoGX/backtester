2d109563ba3a7e4f4f9a06397c95166a0a19b18a
Stella Xiao <sxiao@globalx.ca>
2026-02-18 16:26:13 -0500

    Edit config files and backtest for XIU

--- XIU Example/main_xiu_backtest.py
+++ XIU Example/main_xiu_backtest.py
@@ -2,20 +2,22 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 from pandas.tseries.offsets import BDay
+from xiu_data_pull import fetch_data
 
 start = dt.today() - BDay(60)
 end = dt.today() - BDay(1)
 
+# initialize portfolio
+ticker = "XIU"
+cash = 1000000
+holdings = []
+positions = []
+
 # option data pull
-
-
-
-# initialize portfolio with default cash
-
-
+options_universe = fetch_data("XIU", start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d'), "call", 0.05)
 
 # read and parse portfolio input into dataframe
-
+portfolio = pd.read_csv(r"C:\Users\sxiao\backtester\XIU Example\XIU put call parity.csv")
 
 
 
@@ -30,4 +32,7 @@ while date <= end:
     if date == end:
         return
     else:
-        if 
+        if maturity:
+            return
+        elif ex:
+            return
