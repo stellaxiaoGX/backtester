25ee558dbdf73d7215b56cee90d064e9d21927a9
Stella Xiao <sxiao@globalx.ca>
2026-02-18 10:12:20 -0500

    Added Date Range Flag

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ import os
 cur_dir = os.path.dirname(__file__)
 import sys
 
-# Custom popup dialog class
-class PopupDialog(QDialog):
+# Backtest Popup after running on all inputs
+class BackTestResults(QDialog):
     def __init__(self, message, parent=None):
         super().__init__(parent)
-        self.setWindowTitle("Popup Window")
+        self.setWindowTitle("Results Window")
         self.setFixedSize(250, 120)
 
         # Layout and widgets
@@ -35,28 +35,51 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("Backtester")
         self.setGeometry(0, 0, 400, 500)
         
-        # Button to trigger popup
+        # Final Bottom Button to trigger Backtesting Screen Popup
         self.button = QPushButton("Run Backtest", self)
         self.button.clicked.connect(self.popup_show_backtest_results)
         self.button.resize(120, 40)
         self.button.move(140, 440)
         
+        # Date Range Selection
         self.dateedit1 = QDateEdit(self, calendarPopup=True)
         self.dateedit1.setDateTime(dt.datetime.today() - BDay(5))
+        self.dateedit1.setMaximumDate(dt.datetime.today() - BDay(1))
+        self.dateedit1.dateChanged.connect(self.update_label)
         self.dateedit1.move(70, 10)
         
         self.dateedit2 = QDateEdit(self, calendarPopup=True)
         self.dateedit2.setDateTime(QtCore.QDateTime.currentDateTime())
+        self.dateedit2.setMaximumDate(dt.datetime.today() - BDay(1))
+        self.dateedit2.dateChanged.connect(self.update_label)
         self.dateedit2.move(240, 10)
         
         self.label_start = QLabel('Start Date:', self)
         self.label_start.move(10, 10)
         self.label_end = QLabel('End Date:', self)
         self.label_end.move(180, 10)
+        self.check_dates_label = QLabel("", self)
+        self.check_dates_label.resize(300, 30)
+        self.check_dates_label.move(10, 40)
+        self.update_label()
         
-        self.portfolio_csv = None
-        self.otm_percentage = 0.01
-        self.itm_percentage = 0.01
+        # Portfolio File Input
+        
+        
+        # Backtest Inputs
+        
+        
+        
+    def update_label(self):
+        if self.dateedit1.date() > self.dateedit2.date():
+            self.check_dates_label.setText("Warning: start date must be before end date.")
+            self.check_dates_label.setStyleSheet("color: red;")
+        else:
+            formatted_start_date = self.dateedit1.date().toString("yyyy-dd-MM")
+            formatted_end_date = self.dateedit2.date().toString("yyyy-dd-MM")
+            self.check_dates_label.setText(f"Selected Date Range: {formatted_start_date} to {formatted_end_date}")
+            self.check_dates_label.setStyleSheet("color: black;")
+        
         
     def read_portfolio(self):
         file = self.portfolio_csv
@@ -69,7 +92,7 @@ class MainWindow(QtWidgets.QMainWindow):
         return results
         
     def popup_show_backtest_results(self):
-        popup = PopupDialog("Hello! Here are the backtest results.", self)
+        popup = BackTestResults("Hello! Here are the backtest results.", self)
         popup.exec_()
         
 if __name__ == "__main__":
