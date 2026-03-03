db28141a8bb1c11eb163c39ba208c5a35438deb7
Stella Xiao <sxiao@globalx.ca>
2026-03-03 17:06:36 -0500

    Update portfolio_backtest.py

--- portfolio_backtest.py
+++ portfolio_backtest.py
@@ -10,7 +10,7 @@ else:
     import im_prod.std_lib.common as common
     
 
-port = pd.read_csv(r"C:\Users\sxiao\backtester\portfolio_config\put call parity.csv")
+config = pd.read_csv(r"C:\Users\sxiao\backtester\portfolio_config\put call parity.csv")
 
 class Portfolio():
     """ 
@@ -21,18 +21,27 @@ class Portfolio():
     """
     def __init__(self, config:pd.DataFrame, underlying_ticker:str, country_code:str, start_dt:dt.date(), end_dt:dt.date()):
         
-        ul = underlying_ticker+" "+country_code+" Equity"
-        cur = config['CUR']
-        
-        
         self.db = common.db_connectio()
-        self.underlying = ULAsset(ul, cur)
-        self.cash = Cash(starting_pos, cur)
+        
+        port = config.set_index(config.columns[0], inplace=True)
+        ul_ticker = underlying_ticker+" "+country_code+" Equity"
+        cur = port.loc['cash', 'CUR']
+        starting = config['cash', 'ALLOC']
+        while start_dt in common.tsx_holidays():
+            start_dt = common.workday(start_dt, 1)
+        
+        self.underlying = ULAsset(ul_ticker, cur)
+        self.cash = Cash(starting, cur)
         self.start_dt = start_dt
         self.end_dt = end_dt
         self.currency = cur
-        self.liquidity = liquid_threshold # liquidity of starting cash not allocated
-        return
+        initialized = port.copy()
+        initialized['DATE'] = self.start_dt
+        initialized.loc['underlying', 'TICKER'] = ul_ticker
+        initialized.loc['cash', 'SEC NAME'] = "CAD"
+        initialized['RATE'] = 0.0225
+        
+        self.portfolio = initialized
         
     def daily_market_value_calc(self, today:dt.date()):
         return
